46f0ec01aa66a4e193ad81a42d264ec8f76eb7d4
David Oster aka George Pasparakis <paspa@hotmail.com>
2021-12-13 18:12:00 +0200

    CourseDepartments

---
+++ ContosoUniversity/Areas/DepartmentCourses/Controllers/DepartmentPrivateController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ContosoUniversity.DAL;
+using ContosoUniversity.Areas.DepartmentCourses.Data;
+
+namespace ContosoUniversity.Areas.DepartmentCourses.Controllers
+{
+    public class DepartmentPrivateController : Controller
+    {
+        // GET: DepartmentCourses/DepartmentPrivate
+        public ActionResult Index()
+        {
+            // LINQ 
+            SchoolContext db = new SchoolContext();
+            List<CoursesPerDepartment> DepartmentCourses = new List<CoursesPerDepartment>();
+            db.Departments.Where(d => d.Courses.Count > 0).ToList().ForEach(d => DepartmentCourses.Add(new CoursesPerDepartment() { Department = d, Courses = d.Courses }));
+            ViewBag.DepartmentCourses = DepartmentCourses;
+            return View();
+        }
+    }
+}
---
+++ ContosoUniversity/Areas/DepartmentCourses/Data/CoursesPerDepartment.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ContosoUniversity.Models;
+
+namespace ContosoUniversity.Areas.DepartmentCourses.Data
+{
+    public class CoursesPerDepartment: IDisposable
+    {
+        public Department Department { get; set; }
+        public ICollection<Course> Courses { get; set; }
+
+        public void Dispose()
+        {
+
+        }
+    }
+}
